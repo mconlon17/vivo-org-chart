f2992e816dd4b5187aac579fe12eb540109979fc
Mike Conlon <mconlon@ufl.edu>
2014-08-11 21:36:40 -0400

    Improve code formatting.  Pass org count to display

--- orgs_json.py
+++ orgs_json.py
@@ -13,39 +13,39 @@
         to each org.
     --  Title is now in upper left -- better for on screen viewing.  Name of
         org is now displayed from name of root node rather than hard coded.
+    Version 0.3 MC 2014-08-11
+    --  Remove the tempita dependency
+    --  pylint standard coding
+    --  pass the org_count to D3 for display
 
     To Do
-    --- Pass the number of orgs to the chart.
     --- Add the org home page URI as a parameter.  Make sure every org has a
         value -- D3 doesn't like missing values
-    --- make the tooltip persistent so that you can click on the URIs.
-    --- Make the link in the notes clickable
-    --- Try the hierarchcal display and doing a transform between radial and
+    --- Try the hierarchical display and doing a transform between radial and
         hierarchical
+    --  Start from any org, not just UF (unions to depth)
 
     Future enhancements
     --- How to handle orgs that report to more than one org.  In this version,
         the child is replicated as a child of each of its parents.
 """
 
-__author__      = "Michael Conlon"
-__copyright__   = "Copyright 2013, University of Florida"
-__license__     = "BSD 3-Clause license"
-__version__     = "0.2"
+__author__ = "Michael Conlon"
+__copyright__ = "Copyright 2014, University of Florida"
+__license__ = "BSD 3-Clause license"
+__version__ = "0.3"
 
-import vivotools as vt
+from vivotools import vivo_sparql_query
 import json
-import string
-import tempita
 
-def get_org_parms(uri,orgs):
+def get_org_parms(org_uri, orgs):
     """
-    Given an org uri, find the parms for the org in the orgs json, and identify
-    the uri of the children of the org
+    Given an org_uri, find the parms for the org in the orgs json, and
+    identify the org_uri of the children of the org
     """
     org_parms = {}
     for entry in orgs:
-        if entry["uri"]["value"] == uri:
+        if entry["uri"]["value"] == org_uri:
             for key in entry.keys():
                 if key == "childuri":
                     if "childuri" in org_parms:
@@ -53,34 +53,34 @@ def get_org_parms(uri,orgs):
                     else:
                         org_parms["childuri"] = [entry[key]["value"]]
                 elif key == "type":
-                    type = entry["type"]["value"]
-                    if type.find("Administrative") > 0:
-                        type = "AdministrativeUnit"
-                    elif type.find("NonGovernmentalOrganization") > 0:
-                        type = "NonGovernmentalOrganization"
+                    org_type = entry["type"]["value"]
+                    if org_type.find("Administrative") > 0:
+                        org_type = "AdministrativeUnit"
+                    elif org_type.find("NonGovernmentalOrganization") > 0:
+                        org_type = "NonGovernmentalOrganization"
                     else:
-                        type = type.split("#")[1]
-                    org_parms["type"] = type
+                        org_type = org_type.split("#")[1]
+                    org_parms["type"] = org_type
                 else:
                     org_parms[key] = entry[key]["value"]
     return org_parms
 
-def tree_build(uri,orgs):
+def tree_build(org_uri, orgs):
     """
-    Given uri, the uri of an org in VIVO, and orgs, the JSON list from VIVO,
+    Given the uri of an org in VIVO, and orgs, the JSON list from VIVO,
     take the next step in the recursion to add the org at the uri, and process
     each childuri, if any
     """
-    org_parms = get_org_parms(uri,orgs)
+    org_parms = get_org_parms(org_uri, orgs)
     if "childuri" in org_parms:
         # process children
         org_parms["children"] = []
         for curi in org_parms["childuri"]:
-            if curi != uri:
-                org_parms["children"].append(tree_build(curi,orgs))
+            if curi != org_uri:
+                org_parms["children"].append(tree_build(curi, orgs))
     return org_parms
 
-query = tempita.Template("""
+query = """
     SELECT ?uri (MIN (DISTINCT ?uritype) AS ?type)
         (SAMPLE(DISTINCT ?urilabel) AS ?name) ?childuri
         (COUNT(DISTINCT ?person) AS ?nemployees)
@@ -103,28 +103,24 @@ query = tempita.Template("""
         FILTER (!bound(?successor)) .
         }
         GROUP BY ?uri ?childuri
-        ORDER BY ?uri ?childuri""")
-query = query.substitute()
-
-# Data from file
-# orgs = json.loads(open('orgs-vivo-in.json').read())["results"]["bindings"]
+        ORDER BY ?uri ?childuri"""
 
 # Data from VIVO SPARQL query
 
-orgs = vt.vivo_sparql_query(query)["results"]["bindings"]
+org_result = vivo_sparql_query(query)["results"]["bindings"]
 
 org_dict = {}
-for org in orgs:
+for org in org_result:
     uri = org["uri"]["value"]
-    org_dict[uri] = org_dict.get(uri,0)+1
-print len(org_dict),"orgs"
+    org_dict[uri] = org_dict.get(uri, 0)+1
+print len(org_dict), "orgs"
 
 result = {}
 uri = "http://vivo.ufl.edu/individual/n1278130" # UF
 #uri = "http://vivo.ufl.edu/individual/n8763427" # CTSI
 #uri = "http://vivo.ufl.edu/individual/n142500" # CTSI Biobehavioral core
-result = tree_build(uri,orgs)
-orgs_file = open("orgs.json","w")
-print >>orgs_file,json.dumps(result,indent=4)
+result = tree_build(uri, org_result)
+result["org_count"] = len(org_dict)
+orgs_file = open("orgs.json", "w")
+print >>orgs_file, json.dumps(result, indent=4)
 orgs_file.close()
- 
