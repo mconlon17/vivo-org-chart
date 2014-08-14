bebb24faf12db840d4f3bd2d10ff89e94b821816
Mike Conlon <mconlon@ufl.edu>
2014-08-14 17:38:32 -0400

    Demo and first for testing angular and d3

--- demoApp.js
+++ demoApp.js
@@ -1,1 +1,45 @@
-var app = angular.module('demoApp',[]);
+var d3DemoApp = angular.module('d3DemoApp', [])
+	.directive('dynCircle', function ($parse) {
+
+	var circleData = [{"cx":150,"cy":150,"r":50,"color":"purple"}];
+		
+	return {
+		restrict: 'E',
+		scope: {
+			r: '=',
+		},
+		link: function (scope, element, attrs) {
+
+		// set up initial svg object
+		
+		var viz = d3.select(element[0])
+			.append("svg")
+			.attr("width", 300)
+			.attr("height",300);
+				
+		var circles = viz.selectAll("circle")
+			.data(circleData)
+			.enter()
+			.append("circle")
+				.attr("cx",function(d) {return d.cx;})
+				.attr("cy",function(d) {return d.cy;})
+				.attr("r",function(d) {return d.r;})
+				.style("fill",function(d) {return d.color;});
+
+		scope.$watch('r', function (newVal, oldVal) {
+		
+		alert("In watch. r was "+oldVal+" now "+newVal+"\nWill transition circle.");
+	  
+			viz.selectAll("circle").transition()
+				.duration(750)
+				.delay(200)
+				.attr("r", function() { return newVal; });
+
+		});
+    }
+  }
+});
+
+d3DemoApp.controller('dynCircleController', function dynCircleController ($scope) {
+	$scope.inputr = "75";
+});
